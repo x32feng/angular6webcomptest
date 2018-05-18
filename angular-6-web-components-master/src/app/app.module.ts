6b9fb843fa69331a75f5f3bcaabd351e404c2e23
xuelu feng <x.feng@ibm.com>
2018-05-18 16:24:54 -0400

    added second custom element
    <new-page></...>

--- angular-6-web-components-master/src/app/app.module.ts
+++ angular-6-web-components-master/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 
 import { ButtonComponent } from './button/button.component';
+import { NewpageComponent } from './newpage/newpage.component';
 
 @NgModule({
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent,NewpageComponent],
   imports: [BrowserModule],
-  entryComponents: [ButtonComponent]
+  entryComponents: [ButtonComponent,NewpageComponent]
 })
 export class AppModule {
   constructor(private injector: Injector) {
     const customButton = createCustomElement(ButtonComponent, { injector });
+    
     customElements.define('custom-button', customButton);
+    const newPage = createCustomElement(NewpageComponent, { injector });
+    customElements.define('new-page', newPage);
+    
   }
 
   ngDoBootstrap() {}
---
+++ angular-6-web-components-master/src/app/newpage/newpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'custom-newpage',
+  templateUrl: './newpage.component.html',
+  styleUrls: ['./newpage.component.css']
+})
+export class NewpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

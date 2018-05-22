131bcebe7bb483e6d2a76f0f8be5d4c0ea33420a
xuelu feng <x.feng@ibm.com>
2018-05-22 10:20:35 -0400

    with routes to home

--- angular-6-web-components-master/src/app/app.module.ts
+++ angular-6-web-components-master/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
-
+import {RouterModule} from '@angular/router';
 import { ButtonComponent } from './button/button.component';
 import { NewpageComponent } from './newpage/newpage.component';
-
+import { HomeComponent } from './home/home.component';
+import {APP_BASE_HREF} from '@angular/common';
 @NgModule({
-  declarations: [ButtonComponent,NewpageComponent],
-  imports: [BrowserModule],
-  entryComponents: [ButtonComponent,NewpageComponent]
+  declarations: [ButtonComponent,NewpageComponent, HomeComponent],
+  imports: [BrowserModule,
+  RouterModule.forRoot([
+      {path: 'newpage',
+      component: NewpageComponent},
+      {path:'home',
+      component:HomeComponent}
+  ])],
+  entryComponents: [ButtonComponent,NewpageComponent],
+  providers: [{provide: APP_BASE_HREF, useValue : '/' }]
 })
 export class AppModule {
   constructor(private injector: Injector) {
